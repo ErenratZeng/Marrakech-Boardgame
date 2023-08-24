702ca0ebc227c9100cae368971e9413992881ffa
linzhuiqi <694428549@qq.com>
2023-08-24 21:56:17 +1000

    rename module to model

--- src/comp1110/ass2/model/Board.java
+++ src/comp1110/ass2/model/Board.java
@@ -1,4 +1,4 @@
-package comp1110.ass2.module;
+package comp1110.ass2.model;
 
 public class Board {
     public final static int BOARD_WIDTH = 7;
--- src/comp1110/ass2/model/Dice.java
+++ src/comp1110/ass2/model/Dice.java
@@ -1,4 +1,4 @@
-package comp1110.ass2.module;
+package comp1110.ass2.model;
 import java.util.Random;
 
 public class Dice {
