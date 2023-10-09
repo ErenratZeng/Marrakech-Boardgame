d0cc848694400be908534f4efc7f19eec299d59e
linzhuiqi <694428549@qq.com>
2023-10-09 20:40:34 +1100

    easy AI

--- src/comp1110/ass2/model/Assam.java
+++ src/comp1110/ass2/model/Assam.java
@@ -3,6 +3,8 @@ package comp1110.ass2.model;
 import comp1110.ass2.model.base.IBean;
 import comp1110.ass2.model.base.Point;
 
+import java.util.Random;
+
 import static comp1110.ass2.model.Board.BOARD_HEIGHT;
 import static comp1110.ass2.model.Board.BOARD_WIDTH;
 import static comp1110.ass2.model.base.utils.subStr2int;
@@ -28,8 +30,8 @@ public class Assam implements IBean {
 
     /**
      * @param string: A string where the first character is 'A' (indicating Assam),
-     *              followed by two characters representing the x and y positions,
-     *              and the last character representing the orientation (N, E, S, or W).
+     *                followed by two characters representing the x and y positions,
+     *                and the last character representing the orientation (N, E, S, or W).
      * @description The constructor parses the input string to initialize the position and orientation of Assam.
      * If the orientation character isn't valid, a RuntimeException is thrown.
      */
@@ -51,7 +53,7 @@ public class Assam implements IBean {
     }
 
     /**
-     * @return  Returns an instanc e of Point which represents Assam's position.
+     * @return Returns an instance of Point which represents Assam's position.
      * @description Retrieves the current position of Assam.
      */
     public Point getPoint() {
@@ -79,7 +81,7 @@ public class Assam implements IBean {
 
     /**
      * @param orientation: A value from the Orientation enum indicating Assam's desired direction.
-     * @return  Returns the current instance of the Assam object (useful for method chaining).
+     * @return Returns the current instance of the Assam object (useful for method chaining).
      * @description Updates the direction Assam is facing.
      */
     public Assam setOrientation(Orientation orientation) {
@@ -88,7 +90,7 @@ public class Assam implements IBean {
     }
 
     /**
-     * @return  Returns the current instance of the Assam object (useful for method chaining).
+     * @return Returns the current instance of the Assam object (useful for method chaining).
      * @description Rotates Assam's orientation 90 degrees to the right.
      */
     public Assam setOrientationRight90() {
@@ -127,6 +129,20 @@ public class Assam implements IBean {
             case W -> orientation = Orientation.E;
         }
         return this;
+    }
+
+    public Orientation getOrientationRandomly() {
+        switch (orientation) {
+            case N, S -> {
+                return new Orientation[]{orientation, Orientation.E, Orientation.W}[new Random().nextInt(3)];
+            }
+            case E, W -> {
+                return new Orientation[]{orientation, Orientation.N, Orientation.S}[new Random().nextInt(3)];
+            }
+            default -> throw new RuntimeException(
+                    "Orientation? "+ orientation
+            );
+        }
     }
 
     /**
